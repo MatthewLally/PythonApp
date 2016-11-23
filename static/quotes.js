dcd89a3ddd39768e67c0948ebc4d3a7bd195629b
MatthewLally <mlally8@gmail.com>
2016-11-23 18:23:23 +0000

    Small changes trying to get quotes printing out.....

--- static/quotes.js
+++ static/quotes.js
@@ -1,11 +1,10 @@
-
 var quote = new Array()
-quote[0] = 'HTML for quote number 1'
-quote[1] = 'HTML for quote number 2'
-quote[2] = 'HTML for quote number 3'
-quote[3] = 'HTML for quote number 4'
+    quote[0] = 'HTML for quote number 1'
+    quote[1] = 'HTML for quote number 2'
+    quote[2] = 'HTML for quote number 3'
+    quote[3] = 'HTML for quote number 4'
 
-var randomquote = Math.floor(Math.random()*(quote.length));
-function printquote(){
+    var randomquote = Math.floor(Math.random()*(quote.length));
+    function printquote(){
     document.write(quote[randomquote]);
-}
+    }
