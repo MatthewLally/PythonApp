a1398a06c8c9c07a282764b05b4a10d789407cbb
MatthewLally <mlally8@gmail.com>
2016-11-26 15:12:49 +0000

    Updated Quotes, changed some CSS

--- static/quotes.js
+++ static/quotes.js
@@ -1,9 +1,13 @@
-var quotes = ['The trickster\'s function is to break taboos, create mischief, stir things up. In the end, the trickster gives people what they really want, some sort of freedom. - Tom Robbins',
-  'Disbelief in magic can force a poor soul into believing in government and business. - Tom Robbins',
-  'The trouble with the fast lane is that all the movement is horizontal. And I like to go vertical sometimes. - Tom Robbins',
-  'Our world isn\'t made of earth, air and water or even molecules and atoms; our world is made of language. - Tom Robbins',
-  'I\'m not infatuated with frivolousness. We\'re just good friends. - Tom Robbins',
-  'In fiction, when you paint yourself into a corner, you can write a pair of suction cups onto the bottoms of your shoes and walk up the wall and out the skylight and see the sun breaking through the clouds. In nonfiction, you don\'t have that luxury. - Tom Robbins',]
+var quotes = ['Imagination is more important than knowledge. - Albert Einstein',
+  'If music be the food of love, play on. - William Shakespheare',
+  'The way to get started is to quit talking and begin doing. - Walt Disney',
+  'Obstacles are those frightful things you see when you take your eyes off the goal. - Henry Ford',
+  'I skate where the puck is going to be, not where it has been. - Wayne Gretzky',
+  'When you come to a fork in the road, take it. - Yogi Berra',
+  'We may affirm absolutely that nothing great in the world has been accomplished without passion. - Hegel',
+  'The life which is unexamined is not worth living. - Socrates',
+  'Live as if you were to die tomorrow. Learn as if you were to live forever. - M.K. Gandhi',
+  'What you get by achieving your goals is not as important as what you become by achieving your goals. - Zig Ziglar',]
 function newQuote() {
 var randomNumber = Math.floor(Math.random() * (quotes.length));
 document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
